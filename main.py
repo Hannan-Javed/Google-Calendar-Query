422f05a58991cd64fd846b82901711b7c6e3e861
Hannan-Javed <hannansahib@gmail.com>
2025-01-02 14:50:37 +0500

    handle if database already exists

--- main.py
+++ main.py
@@ -4,21 +4,27 @@ from database_manager import DatabaseManager
 from utils import filter_events
 from config import TIME_ZONE
 
-def main():    
-    calendar_service = GoogleCalendarService()
-    start_date = "01-12-2024"
-    end_date = "31-12-2024"
-
-    start_date = dt.datetime.strptime(start_date, "%d-%m-%Y")
-    start_date = TIME_ZONE.localize(start_date).isoformat()
-    end_date = dt.datetime.strptime(end_date, "%d-%m-%Y")
-    end_date = TIME_ZONE.localize(end_date).isoformat()
-
-    all_events = calendar_service.fetch_events(start_date, end_date)
-    all_events = filter_events(all_events)
+def main():
     
     db_manager = DatabaseManager()
-    db_manager.build_database(all_events)
+    if db_manager.database_exists():
+        create_new_database = input("Database already exists. Do you want to create a new one and insert data, or use existing one? (new/old): ")
+        if create_new_database == "new":
+            db_manager.create_database()
+            # fetch events from Google Calendar and build the database
+            calendar_service = GoogleCalendarService()
+            start_date = "01-12-2024"
+            end_date = "31-12-2024"
+
+            start_date = dt.datetime.strptime(start_date, "%d-%m-%Y")
+            start_date = TIME_ZONE.localize(start_date).isoformat()
+            end_date = dt.datetime.strptime(end_date, "%d-%m-%Y")
+            end_date = TIME_ZONE.localize(end_date).isoformat()
+
+            all_events = calendar_service.fetch_events(start_date, end_date)
+            all_events = filter_events(all_events)
+            
+            db_manager.build_database(all_events)
     
     # sample query
     query = "SELECT SUM(duration) FROM CALENDAR WHERE colorId=6 GROUP BY colorId;"
