cdb9479e86d5c8d44ee424b2eeb2c6cad958dfa4
Hannan-Javed <hannansahib@gmail.com>
2025-03-11 14:40:36 +0800

    fix: compare each corresponding event from database and calendar (if length is same) to look for change instead of only comparing date

--- database_manager.py
+++ database_manager.py
@@ -15,10 +15,8 @@ class DatabaseManager:
         result = cursor.fetchone()
 
         if result is not None:
-            db_events = self.con.execute("SELECT startDate FROM CALENDAR;").fetchall()
-            if db_events is not None:
-                db_start_date = db_events[0][0]
-                db_last_date = db_events[-1][0] 
+            cursor = self.con.execute("SELECT * FROM CALENDAR;")
+            db_events = [dict(zip([column[0] for column in cursor.description], row)) for row in cursor.fetchall()]
 
         calendar_service = GoogleCalendarService()
 
@@ -34,12 +32,19 @@ class DatabaseManager:
             print("No events found from start date till end date")
             sys.exit()
 
-        calendar_start_date = all_events[0]['startDate'].strftime('%d-%m-%Y')
-        calendar_last_date = all_events[-1]['startDate'].strftime('%d-%m-%Y')
-
-        if result is None or db_events is None or calendar_start_date != db_start_date or calendar_last_date != db_last_date:
+        if result is None or db_events is None or len(all_events) != len(db_events):
             self.create_table()
             self.build_database(all_events)
+        else:
+            changed = False
+            for db_event, calendar_event in zip(db_events, all_events):
+                if db_event != calendar_event:
+                    changed = True
+                    break
+
+            if changed:
+                self.create_table()
+                self.build_database(all_events)
 
     def create_table(self):
         self.con.execute("DROP TABLE IF EXISTS CALENDAR;")
